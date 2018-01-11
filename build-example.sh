3b3da37c732587d961ed587580ac2309eda0e052
Leonard Lee <sheeeng@gmail.com>
2018-01-11 11:04:45 +0100

    Separate example build.

---
+++ build-example.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -x
+
+export DEBIAN_FRONTEND=noninteractive
+export WORKSPACE_DIR=/workspace
+export BUILD_DIR=${WORKSPACE_DIR}/Build
+export BUILD_TARGET_DIR=${WORKSPACE_DIR}/Source/Example
+
+if [ "$PWD" != "$WORKSPACE_DIR" ]; then
+  echo "Build helper script to be run inside a container."
+  echo "Go to $WORKSPACE_DIR directory before running this script."
+  exit 1
+fi
+
+cd $BUILD_TARGET_DIR \
+  && ${BUILD_DIR}/build.sh
